d0faf5fb677b8801b53ca66d95022799d4a028a5
Demetre Panjakidze <Dpanj2021@agruni.edu.ge>
2023-02-12 15:54:21 +0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, OnInit, Component } from '@angular/core';
+import { fromEvent, Observable, of } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'remembering';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const obs$ = new Observable((x) => {
+      const random = Math.random();
+      x.next(random);
+    });
+
+    obs$.subscribe(console.log);
+    obs$.subscribe(console.log);
+  }
 }
